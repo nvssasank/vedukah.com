d30c9f197cc2bb0bb669035e68f090afdccb5a8f
nvssasank <109466773+nvssasank@users.noreply.github.com>
2023-04-05 20:27:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// document.getElementById('login_id').style.display="inline-block";
+const navBtn = document.getElementById('nav-btn');
+const cancelBtn = document.getElementById('cancel-btn');
+const sideNav = document.getElementById('sidenav');
+const modal = document.getElementById('modal');
+// const loginId = document.getElementById('some');
+
+navBtn.addEventListener("click", function(){
+    sideNav.classList.add('show');
+    modal.classList.add('showModal');
+});
+
+cancelBtn.addEventListener('click', function(){
+    sideNav.classList.remove('show');
+    modal.classList.remove('showModal');
+});
+
+window.addEventListener('click', function(event){
+    if(event.target === modal){
+        sideNav.classList.remove('show');
+        modal.classList.remove('showModal');
+    }
+});
+
